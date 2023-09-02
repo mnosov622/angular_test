e1db07f29369a9dcef9da6fbd2f6ce8611d45bf7
Maxim N <mnosov622@gmail.com>
2023-09-02 01:27:45 -0400

    display selected music in mp3-player component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MusicListComponent } from './music-list/music-list.component';
+import { Mp3PlayerComponent } from './mp3-player/mp3-player.component';
 
 @NgModule({
-  declarations: [AppComponent, MusicListComponent],
+  declarations: [AppComponent, MusicListComponent, Mp3PlayerComponent],
   imports: [BrowserModule, MatTableModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
